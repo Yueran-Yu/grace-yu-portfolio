d152bb591c6c5e5e194ee3780fa11a06d1885b92
GraceYu <librayuyueran@gmail.com>
2022-01-16 08:05:24 -0600

    complete the toggle function

--- src/context/ToggleContextProvider.jsx
+++ src/context/ToggleContextProvider.jsx
@@ -2,13 +2,15 @@ import React, {createContext, useState} from "react";
 
 export const ThemeContext = createContext('')
 
-export const DayNightSwitchProvider = ({children})=>{
+export const ToggleContext = ({children})=>{
   const [theme, setTheme] = useState('light')
 
   const themeToggler = () => {
     // console.log(theme)
     theme === 'light' ? setTheme('dark') : setTheme('light')
   }
+
+
 
   return(
     <ThemeContext.Provider value={{theme, themeToggler}}>
